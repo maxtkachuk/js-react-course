67b93d4c7b043f534aa86531f399fb4c64ef4a73
Maksym <max.tkachukk01@gmail.com>
2023-03-10 22:45:20 +0200

    Events and their handlers

---
+++ js/testJS/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const btns = document.querySelector('button'),
+      overlay = document.querySelector('.overlay');
+// btn.onclick = function(){
+//     alert('Clicked!');
+// };
+
+// btn.addEventListener('click', () => {
+//     alert('Clicked');
+// });
+
+// btn.addEventListener('mouseenter', (event) => {
+//     console.log(event.target);
+//     event.target.remove();
+// });
+
+// let i = 0;
+const deleteElement = (e) => {
+    console.log(e.target);
+    console.log(e.type);
+    // i++;
+
+    // if(i == 1) {
+    //     btn.removeEventListener('click', deleteElement);
+    // }
+};
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement, {once: true});
+});
+
+const link = document.querySelector('a');
+
+link.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    console.log(event.target);
+});
+
