dc52008954fe37d258477f520a6e1a21f7bcca42
Maksym <max.tkachukk01@gmail.com>
2023-03-17 19:45:23 +0200

    Practice №2

---
+++ js/practice/pract-2/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var storeName = "Veles";
+var storeDescription = {
+    budget: 10000,
+    employees: ['John', 'Alex','Bob'],
+    products: {
+        sneakers: 500,
+        hoodie: 300
+    },
+    open: true
+};
