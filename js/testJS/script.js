2e6e40e721bfa96a9fd17a9eaf3e2ca26ee1964f
Maksym <max.tkachukk01@gmail.com>
2023-03-23 01:56:29 +0200

    Events on mobile devices

--- js/testJS/script.js
+++ js/testJS/script.js
@@ -1,106 +1,35 @@
 'use strict';
 
-function pow(x, n){
-    let result = 1;
+//touchstart
+//touchmove
+//touchend
+//touchenter
+//touchleave
+//touchcancel
 
-    for(let i = 0; i < n; i++){
-        result *= x;
-    }    
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
 
-    return result;
-}
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
 
-function pow(x, n) {
-    if (n == 1) {
-      return x;
-    } else {
-      return x * pow(x, n - 1);
-    }
-}
+        console.log('Start!');
+        console.log(e.targetTouches);
+    });
 
-console.log(pow(2,3));
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
 
-let students = {
-    js: [{
-        name: 'John',
-        progress: 100
-    }, {
-        name: 'Ivan',
-        progress: 60
-    }],
+        console.log(e.targetTouches[0].pageX);
+    });
 
-    html: {
-        basic: [{
-            name: 'Peter',
-            progress: 20
-        }, {
-            name: 'Ann',
-            progress: 18
-        }],
+    box.addEventListener('touchend', (e) => {
+        e.preventDefault();
 
-        pro: [{
-            name: 'Sam',
-            progress: 10
-        }],
+        console.log('End!');
+    });
+});
 
-        semi: {
-            students: [{
-                name: 'Test',
-                progress: 100
-            }]
-        }
-    }
-};
-
-function getTotalProgressByIteration(data){
-    let total = 0;
-    let students = 0;
-
-    for(let course of Object.values(data)){
-        if(Array.isArray(course)){
-            students += course.length;
-
-            for(let i = 0; i < course.length; i++){
-                total += course[i].progress;
-            }
-        } else {
-            for(let subCourse of Object.values(course)){
-                students += subCourse.length;
-
-                for(let i = 0; i < subCourse.length; i++){
-                    total += subCourse[i].progress;
-                }
-            }
-        }
-    }
-
-    return total / students;
-}
-
-console.log(getTotalProgressByIteration(students));
-
-function getTotalProgressByRecursion(data){
-    if(Array.isArray(data)){
-        let total = 0;
-
-        for(let i = 0; i < data.length; i++){
-            total += data[i].progress;
-        }
-
-        return [total, data.length];
-    } else {
-        let total = [0, 0];
-
-        for(let subData of Object.values(data)){
-            const subDataArr = getTotalProgressByRecursion(subData);
-            total[0] += subDataArr[0];
-            total[1] += subDataArr[1];
-        }
-
-        return total;
-    }
-}
-
-const result = getTotalProgressByRecursion(students);
-
-console.log(result[0] / result[1]);
+// touches
+// targetTouches
+// changedTouches
