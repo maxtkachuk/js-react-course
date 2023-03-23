039a34d6a703ebaea79b9301fc3552913e3b966b
Maksym <max.tkachukk01@gmail.com>
2023-03-24 00:58:51 +0200

    Homework №1

--- js/homework/js/script.js
+++ js/homework/js/script.js
@@ -1,3 +1,22 @@
 'use strict';
 //HOMEWORK
 
+const numberOfFilms = +prompt('How much films have you watched?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('What movie did you watch recently?', ''),
+      b = prompt('Rate it:', ''),
+      c = prompt('What movie did you watch recently?', ''),
+      d = prompt('Rate it:', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
