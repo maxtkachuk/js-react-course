949a781872eb6035a906976fd82f38e789f9d5d8
Maksym <max.tkachukk01@gmail.com>
2023-03-24 01:13:56 +0200

    Homework №2

--- js/homework/js/script.js
+++ js/homework/js/script.js
@@ -19,4 +19,27 @@ const a = prompt('What movie did you watch recently?', ''),
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
+for(let i = 0; i < 2; i++){
+    const nameOfFilm = prompt('What movie did you watch recently?', '');
+    const markOfFilm = prompt('Rate it:', '');
+
+    if(nameOfFilm != null && markOfFilm != null && nameOfFilm != '' && markOfFilm != '' && nameOfFilm.length < 50){
+        personalMovieDB[nameOfFilm] = markOfFilm;
+        console.log('Done!');
+    } else {
+        console.log("Error!");
+        i--;
+    }
+}
+
+if(personalMovieDB.count < 10){
+    console.log('Watched very few films.');
+} else if(personalMovieDB.actors >= 10 && personalMovieDB < 30){
+    console.log('You are a classic viewer,');
+} else if(personalMovieDB.count > 30){
+    console.log('You are a cinephile!');
+} else {
+    console.log('Error!');
+}
+
 console.log(personalMovieDB);
