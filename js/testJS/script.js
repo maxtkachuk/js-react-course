1aea777e1c2f57b067118a562e592439afaa2fac
Maksym <max.tkachukk01@gmail.com>
2023-04-25 21:16:58 +0300

    Function constructor

--- js/testJS/script.js
+++ js/testJS/script.js
@@ -1,13 +1,25 @@
 'use strict';
 
-const box = document.querySelector('.box');
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}!`);
+    };
+}
 
-let observer = new MutationObserver(MutationRecords => {
-    console.log(MutationRecords);
-});
+User.prototype.exit = function(){
+    console.log(`User ${this.name} left.`);
+};
 
-observer.observe(box, {
-    childList: true
-});
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
 
-observer.disconnect();
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
