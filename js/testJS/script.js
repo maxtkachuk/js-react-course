d1bf6b1e786acd33c3cd8890ee7a1e2b04ccf776
Maksym <max.tkachukk01@gmail.com>
2023-04-25 22:43:24 +0300

    Classes

--- js/testJS/script.js
+++ js/testJS/script.js
@@ -1,83 +1,35 @@
 'use strict';
 
-// function showThis(a, b) {
-//     console.log(this);
-//     function sum() {
-//         console.log(this);
-//         return a + b;
-//     }
-
-//     console.log(sum());
-// }
-
-// showThis(4, 5);
-
-// const obj = {
-//     a: 20,
-//     b: 15,
-//     sum: function() {
-//         function shout(){
-//             console.log(this);
-//         }
-//         shout();
-//     }
-// };
-
-// obj.sum();
-
-// function User(name, id) {
-//     this.name = name;
-//     this.id = id;
-//     this.human = true;
-//     this.hello = function() {
-//         console.log("Hello" + this.name);
-//     };
-// }
-
-// let ivan = new User('Ivan', 23);
-
-// function sayName(surname) {
-//     console.log(this);
-//     console.log(this.name + surname);
-// }
-
-// const user = {
-//     name: 'John'
-// };
-
-// sayName.call(user, 'Smith');
-// sayName.apply(user, ['Smith']);
-
-// function count(num) {
-//     return this*num;
-// }
-
-// const double = count.bind(3);
-// console.log(double(3));
-// console.log(double(13));
-
-// 1) Звичайна функція: this = window, но якщо стоїть use strict - то буде undefined
-// 2) Контекст у методів об'єкта - сам об'єкт.
-// 3) this у конструкторах і класах - це новий екземпляр об'єкта.
-// 4) Ручна прив'язка this: call, apply, bind
-
-const btn = document.querySelector('button');
-
-btn.addEventListener('click', (e) => {
-    e.target.style.background = "red";
-});
-
-const obj = {
-    num: 5,
-    sayNumber: function() {
-        const say = () => {
-            console.log(this.num);
-        };
-        say();
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
     }
-};
 
-obj.sayNumber();
+    calcArea() {
+        return this.height * this.width;
+    }
+}
 
-const double = a => a * 2;
-console.log(double(4));
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+    
+    showMyProps() {
+        console.log(`Text: ${this.text}, color: ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWithText(25, 10, 'Some text!', 'red');
+
+div.showMyProps();
+console.log(div.calcArea());
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
